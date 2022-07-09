66a30ee456c49d969cb7fc01a7221b0fff04dece
unknown <sivams20@gmail.com>
2022-07-09 08:40:31 +0530

    created mongodb database, connected to db

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+import mongoose from "mongoose";
 
 const app = express();
 const port = 5000;
 
 app.listen(port, ()=>{
     console.log(`Connected successfully on port ${port}`);
+});
+
+mongoose.connect('mongodb+srv://sivams20:siv_admin@expensetrackcluster.lvlsc.mongodb.net/?retryWrites=true&w=majority').then(() => {
+    console.log('Connected to database !!');
+}).catch((err) => {
+    console.log('Connection failed!' + err.message);
 });
